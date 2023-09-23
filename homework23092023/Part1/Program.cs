12c6a995c01077ea9564ad11c3f44199ae96c7be
c0nf1dxnt <c0nf1dxnt@yandex.ru>
2023-09-24 01:17:19 +0300

    1:17 AM 9/24/2023 version

--- homework23092023/Part1/Program.cs
+++ homework23092023/Part1/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Runtime.Versioning;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Part1
 {
     internal class Program
     {
+        static void Task1()
+        {
+            short day = 0;
+            while (true)
+            {
+                Console.Write("Введите число: ");
+                day = short.Parse(Console.ReadLine());
+                if (day <= 365 && day > 0)
+                    break;
+                else
+                    Console.Clear();
+            }
+            DateTime date = new DateTime(1, 1, 1);
+            for (int i = 1; i < 366; i++)
+            {
+                Console.WriteLine($"Число {i} соответствует: {date.AddDays(i - 1).ToString("d MMMM")}");
+            }
+        }
         static void Main(string[] args)
         {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Task1();
+
+            Console.ReadKey();
         }
     }
 }
---
+++ homework23092023/Part2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace Part2
+{
+    internal class Program
+    {
+        static void Task1()
+        {
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Task1();
+
+            Console.ReadKey();
+        }
+    }
+}
